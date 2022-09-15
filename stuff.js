056ad9a867ba2e40a3846ffe76ce0a44acb37f23
Jeimy <113571060+Jeimy-SM@users.noreply.github.com>
2022-09-14 23:24:14 -0400

    Add files via upload

---
+++ stuff.js
@@ -0,0 +1,13 @@
+
+
+
+
+<form>
+<input type=button
+value="Press to play anthem"
+onClick="self.location= 'https://www.youtube.com/watch?v=gFbHfXXaYCc'">
+/*</form>
+<video width="450" height="350" controls>
+<source src="yes.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>*/
